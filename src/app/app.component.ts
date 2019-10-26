a4029fe6c5247913bcf975e14092748e30090719
Dilyana Mihaylova <dilyana.p.mihaylova@gmail.com>
2019-10-26 20:27:32 +0300

    move logic to grid component; refactor checks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,109 +8,108 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title: string = 'Game of Life';
 
-  rows: number = 20;
-  cols: number = 50;
+  // rows: number = 20;
+  // cols: number = 50;
 
-  playing: boolean = false;
+  // playing: boolean = false;
 
-  currentGeneration: Array<number[]> = new Array(this.rows);
-  nextGeneration: Array<number[]> = new Array(this.rows);
+  // currentGeneration: Array<number[]> = new Array(this.rows);
+  // nextGeneration: Array<number[]> = new Array(this.rows);
 
-  timer;
-  reproductionTime: number = 100;
+  // timer;
+  // reproductionTime: number = 100;
 
-  initializeGrid = () => {
-    for (let i = 0; i < this.rows; i++) {
-      this.currentGeneration[i] = new Array(this.cols);
-      this.nextGeneration[i] = new Array(this.cols);
-    }
-  }
+  // initializeGrid = () => {
+  //   for (let i = 0; i < this.rows; i++) {
+  //     this.currentGeneration[i] = new Array(this.cols);
+  //     this.nextGeneration[i] = new Array(this.cols);
+  //   }
+  // }
 
-  countNeighbors = (row: number, col: number) => {
-    let count = 0;
-    if (row - 1 >= 0) {
-      if (this.currentGeneration[row - 1][col] === 1) count++;
-    }
-    if (row - 1 >= 0 && col - 1 >= 0) {
-      if (this.currentGeneration[row - 1][col - 1] === 1) count++;
-    }
-    if (row - 1 >= 0 && col + 1 < this.cols) {
-      if (this.currentGeneration[row - 1][col + 1] === 1) count++;
-    }
-    if (col - 1 >= 0) {
-      if (this.currentGeneration[row][col - 1] === 1) count++;
-    }
-    if (col + 1 < this.cols) {
-      if (this.currentGeneration[row][col + 1] === 1) count++;
-    }
-    if (row + 1 < this.rows) {
-      if (this.currentGeneration[row + 1][col] === 1) count++;
-    }
-    if (row + 1 < this.rows && col - 1 >= 0) {
-      if (this.currentGeneration[row + 1][col - 1] === 1) count++;
-    }
-    if (row + 1 < this.rows && col +1  < this.cols) {
-      if (this.currentGeneration[row + 1][col + 1] === 1) count++;
-    }
-    return count;
-  }
+  // countNeighbors = (row: number, col: number) => {
+  //   let count = 0;
+  //   if (row - 1 >= 0) {
+  //     if (this.currentGeneration[row - 1][col] === 1) count++;
+  //   }
+  //   if (row - 1 >= 0 && col - 1 >= 0) {
+  //     if (this.currentGeneration[row - 1][col - 1] === 1) count++;
+  //   }
+  //   if (row - 1 >= 0 && col + 1 < this.cols) {
+  //     if (this.currentGeneration[row - 1][col + 1] === 1) count++;
+  //   }
+  //   if (col - 1 >= 0) {
+  //     if (this.currentGeneration[row][col - 1] === 1) count++;
+  //   }
+  //   if (col + 1 < this.cols) {
+  //     if (this.currentGeneration[row][col + 1] === 1) count++;
+  //   }
+  //   if (row + 1 < this.rows) {
+  //     if (this.currentGeneration[row + 1][col] === 1) count++;
+  //   }
+  //   if (row + 1 < this.rows && col - 1 >= 0) {
+  //     if (this.currentGeneration[row + 1][col - 1] === 1) count++;
+  //   }
+  //   if (row + 1 < this.rows && col +1  < this.cols) {
+  //     if (this.currentGeneration[row + 1][col + 1] === 1) count++;
+  //   }
+  //   return count;
+  // }
 
-  applyRules = (row: number, col: number) => {
-    let numNeighbors = this.countNeighbors(row, col);
-    if (this.currentGeneration[row][col] === 1) {
-      if (numNeighbors < 2) {
-        this.nextGeneration[row][col] = 0;
-      } else if (numNeighbors === 2 || numNeighbors === 3) {
-        this.nextGeneration[row][col] = 1;
-      } else if (numNeighbors > 3) {
-        this.nextGeneration[row][col] = 0;
-      }
-    } else if (this.currentGeneration[row][col] === 0) {
-      if (numNeighbors === 3) {
-        this.nextGeneration[row][col] = 1;
-      }
-    }
-  }
+  // applyRules = (row: number, col: number) => {
+  //   let numNeighbors = this.countNeighbors(row, col);
+  //   if (this.currentGeneration[row][col] === 1) {
+  //     if (numNeighbors < 2) {
+  //       this.nextGeneration[row][col] = 0;
+  //     } else if (numNeighbors === 2 || numNeighbors === 3) {
+  //       this.nextGeneration[row][col] = 1;
+  //     } else if (numNeighbors > 3) {
+  //       this.nextGeneration[row][col] = 0;
+  //     }
+  //   } else if (this.currentGeneration[row][col] === 0) {
+  //     if (numNeighbors === 3) {
+  //       this.nextGeneration[row][col] = 1;
+  //     }
+  //   }
+  // }
 
-  copyAndResetGrid = () => {
-    for (let i = 0; i < this.rows; i++) {
-      for (let j = 0; j < this.cols; j++) {
-        this.currentGeneration[i][j] = this.nextGeneration[i][j];
-        this.nextGeneration[i][j] = 0;
-      }
-    }
-  }
+  // copyAndResetGrid = () => {
+  //   for (let i = 0; i < this.rows; i++) {
+  //     for (let j = 0; j < this.cols; j++) {
+  //       this.currentGeneration[i][j] = this.nextGeneration[i][j];
+  //       this.nextGeneration[i][j] = 0;
+  //     }
+  //   }
+  // }
 
-  play = () => {
-    this.computeNextGen();
+  // play = () => {
+  //   // this.computeNextGen();
+  //   if (this.playing) {
+  //     this.timer = setTimeout(this.play, this.reproductionTime);
+  //   }
+  // }
 
-    if (this.playing) {
-      this.timer = setTimeout(this.play, this.reproductionTime);
-    }
-  }
+  // computeNextGen = () => {
+  //   for (let i = 0; i < this.rows; i++) {
+  //     for (let j = 0; j < this.cols; j++) {
+  //       this.applyRules(i, j);
+  //     }
+  //   }
+  //   this.copyAndResetGrid();
+  // }
 
-  computeNextGen = () => {
-    for (let i = 0; i < this.rows; i++) {
-      for (let j = 0; j < this.cols; j++) {
-        this.applyRules(i, j);
-      }
-    }
-    this.copyAndResetGrid();
-  }
-
-  initialize = () => {
-    this.initializeGrid();
-    this.playing = true;
-    this.play();
-    for (let i = 0; i < this.rows; i++) {
-      for (let j = 0; j < this.cols; j++) {
-        let isLive = Math.round(Math.random());
-        if (isLive === 1) {
-          this.currentGeneration[i][j] = 1;
-        } else {
-          this.currentGeneration[i][j] = 0;
-        }
-      }
-    }
-  }
+  // initialize = () => {
+  //   // this.initializeGrid();
+  //   this.playing = true;
+  //   this.play();
+  //   // for (let i = 0; i < this.rows; i++) {
+  //   //   for (let j = 0; j < this.cols; j++) {
+  //   //     let isLive = Math.round(Math.random());
+  //   //     if (isLive === 1) {
+  //   //       this.currentGeneration[i][j] = 1;
+  //   //     } else {
+  //   //       this.currentGeneration[i][j] = 0;
+  //   //     }
+  //   //   }
+  //   // }
+  // }
 }
--- src/app/components/grid/grid.component.ts
+++ src/app/components/grid/grid.component.ts
@@ -12,6 +12,10 @@ export class GridComponent implements OnInit {
   currentGeneration: Array<number[]> = new Array(this.rows);
   nextGeneration: Array<number[]> = new Array(this.rows);
 
+  playing: boolean = false;
+  timer;
+  reproductionTime: number = 100;
+
   ngOnInit() {
     this.initializeGrid();
     this.setupCells();
@@ -34,6 +38,94 @@ export class GridComponent implements OnInit {
           this.currentGeneration[i][j] = 0;
         }
       }
+    }
+  }
+
+  countAliveNeighbours = (row: number, col: number) => {
+    let count = 0;
+
+    // check neighbour above current cell
+    if (row - 1 >= 0 && this.currentGeneration[row - 1][col] === 1) {
+      count++;
+    }
+    // check neighbour on top right of current cell
+    if (row - 1 >= 0 && col + 1 < this.cols
+      && this.currentGeneration[row - 1][col + 1] === 1) {
+      count++;
+    }
+    // check neighbour on right of current cell
+    if (col + 1 < this.cols && this.currentGeneration[row][col + 1] === 1) {
+      count++;
+    }
+    // check neighbour on bottom right of current cell
+    if (row + 1 < this.rows && col + 1 < this.cols
+      && this.currentGeneration[row + 1][col + 1] === 1) {
+      count++;
+    }
+    // check neighbour below current cell
+    if (row + 1 < this.rows && this.currentGeneration[row + 1][col] === 1) {
+      count++;
+    }
+    // check neighbour on bottom left of current cell
+    if (row + 1 < this.rows && col - 1 >= 0
+      && this.currentGeneration[row + 1][col - 1] === 1) {
+      count++;
+    }
+    // check neighbour on left of current cell
+    if (col - 1 >= 0 && this.currentGeneration[row][col - 1] === 1) {
+      count++;
+    }
+    // check neighbour on top left of current cell
+    if (row - 1 >= 0 && col - 1 >= 0
+      && this.currentGeneration[row - 1][col - 1] === 1) {
+      count++;
+    }
+    return count;
+  }
+
+  applyRules = (row: number, col: number) => {
+    let aliveNeihgbours = this.countAliveNeighbours(row, col);
+
+    if (this.currentGeneration[row][col] === 1) {
+      if (aliveNeihgbours < 2 || aliveNeihgbours > 3) {
+        this.nextGeneration[row][col] = 0;
+      } else if (aliveNeihgbours === 2 || aliveNeihgbours === 3) {
+        this.nextGeneration[row][col] = 1;
+      }
+    } else if (this.currentGeneration[row][col] === 0) {
+      if (aliveNeihgbours === 3) {
+        this.nextGeneration[row][col] = 1;
+      }
+    }
+  }
+
+  replaceGenerationAndResetGrid = () => {
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        this.currentGeneration[i][j] = this.nextGeneration[i][j];
+        this.nextGeneration[i][j] = 0;
+      }
+    }
+  }
+
+  computeNextGeneration = () => {
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        this.applyRules(i, j);
+      }
+    }
+    this.replaceGenerationAndResetGrid();
+  }
+
+  startGame = () => {
+    this.playing = true;
+    this.play();
+  }
+
+  play = () => {
+    this.computeNextGeneration();
+    if (this.playing) {
+      this.timer = setTimeout(this.play, this.reproductionTime);
     }
   }
 
