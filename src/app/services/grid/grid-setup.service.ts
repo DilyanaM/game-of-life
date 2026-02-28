db59465f4ae5450a636c3ec94ec8598780ca2730
Dilyana Mihaylova <dilyana.p.mihaylova@gmail.com>
2026-02-28 15:34:40 +0200

    Add tests for GridSetupService

--- src/app/services/grid/grid-setup.service.ts
+++ src/app/services/grid/grid-setup.service.ts
@@ -9,26 +9,26 @@ export class GridSetupService {
 
   private currentGenerationSubject = new BehaviorSubject<number[][]>([]);
   private nextGenerationSubject = new BehaviorSubject<number[][]>([]);
-  private currentGeneration = new Array<number[]>(GRID_DIMENSIONS.rows);
-  private nextGeneration = new Array<number[]>(GRID_DIMENSIONS.rows);
+  private currentGeneration = new Array<number[]>();
+  private nextGeneration = new Array<number[]>();
 
   constructor() {
     this.currentGeneration$ = this.currentGenerationSubject.asObservable();
     this.nextGeneration$ = this.nextGenerationSubject.asObservable();
   }
 
-  initializeGrid(): void {
-    for (let i = 0; i < GRID_DIMENSIONS.rows; i++) {
-      this.currentGeneration[i] = new Array(GRID_DIMENSIONS.cols);
-      this.nextGeneration[i] = new Array(GRID_DIMENSIONS.cols);
+  initializeGrid(columns = GRID_DIMENSIONS.cols, rows = GRID_DIMENSIONS.rows): void {
+    for (let i = 0; i < rows; i++) {
+      this.currentGeneration[i] = new Array(columns);
+      this.nextGeneration[i] = new Array(columns);
     }
     this.currentGenerationSubject.next(this.currentGeneration);
     this.nextGenerationSubject.next(this.nextGeneration);
   }
 
-  setupCells(): void {
-    for (let i = 0; i < GRID_DIMENSIONS.rows; i++) {
-      for (let j = 0; j < GRID_DIMENSIONS.cols; j++) {
+  setupCells(columns = GRID_DIMENSIONS.cols, rows = GRID_DIMENSIONS.rows): void {
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < columns; j++) {
         const isAlive = Math.round(Math.random());
         if (isAlive === 1) {
           this.currentGeneration[i][j] = 1;
@@ -40,77 +40,82 @@ export class GridSetupService {
     this.currentGenerationSubject.next(this.currentGeneration);
   }
 
-  computeNextGeneration(): void {
-    for (let i = 0; i < GRID_DIMENSIONS.rows; i++) {
-      for (let j = 0; j < GRID_DIMENSIONS.cols; j++) {
+  computeNextGeneration(columns = GRID_DIMENSIONS.cols, rows = GRID_DIMENSIONS.rows): void {
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < columns; j++) {
         this.applyRules(i, j);
       }
     }
     this.replaceGenerationAndResetGrid();
   }
 
-  private countAliveNeighbours(row: number, col: number): number {
+  private countAliveNeighbours(
+    row: number,
+    column: number,
+    rows = GRID_DIMENSIONS.rows,
+    columns = GRID_DIMENSIONS.cols
+  ): number {
     let count = 0;
 
     // check neighbour above current cell
-    if (row - 1 >= 0 && this.currentGeneration[row - 1][col] === 1) {
+    if (row - 1 >= 0 && this.currentGeneration[row - 1][column] === 1) {
       count++;
     }
     // check neighbour on top right of current cell
-    if (row - 1 >= 0 && col + 1 < GRID_DIMENSIONS.cols
-      && this.currentGeneration[row - 1][col + 1] === 1) {
+    if (row - 1 >= 0 && column + 1 < columns
+      && this.currentGeneration[row - 1][column + 1] === 1) {
       count++;
     }
     // check neighbour on right of current cell
-    if (col + 1 < GRID_DIMENSIONS.cols && this.currentGeneration[row][col + 1] === 1) {
+    if (column + 1 < columns && this.currentGeneration[row][column + 1] === 1) {
       count++;
     }
     // check neighbour on bottom right of current cell
-    if (row + 1 < GRID_DIMENSIONS.rows && col + 1 < GRID_DIMENSIONS.cols &&  this.currentGeneration[row + 1][col + 1] === 1) {
+    if (row + 1 < rows && column + 1 < columns &&  this.currentGeneration[row + 1][column + 1] === 1) {
       count++;
     }
     // check neighbour below current cell
-    if (row + 1 < GRID_DIMENSIONS.rows && this.currentGeneration[row + 1][col] === 1) {
+    if (row + 1 < rows && this.currentGeneration[row + 1][column] === 1) {
       count++;
     }
     // check neighbour on bottom left of current cell
-    if (row + 1 < GRID_DIMENSIONS.rows && col - 1 >= 0 && this.currentGeneration[row + 1][col - 1] === 1) {
+    if (row + 1 < rows && column - 1 >= 0 && this.currentGeneration[row + 1][column - 1] === 1) {
       count++;
     }
     // check neighbour on left of current cell
-    if (col - 1 >= 0 && this.currentGeneration[row][col - 1] === 1) {
+    if (column - 1 >= 0 && this.currentGeneration[row][column - 1] === 1) {
       count++;
     }
     // check neighbour on top left of current cell
-    if (row - 1 >= 0 && col - 1 >= 0 && this.currentGeneration[row - 1][col - 1] === 1) {
+    if (row - 1 >= 0 && column - 1 >= 0 && this.currentGeneration[row - 1][column - 1] === 1) {
       count++;
     }
 
     return count;
   }
 
-  private applyRules(row: number, col: number): void {
-    const aliveNeihgbours = this.countAliveNeighbours(row, col);
-    const isAlive = this.currentGeneration[row][col] === 1;
-    const isDead = this.currentGeneration[row][col] === 0;
+  private applyRules(row: number, column: number): void {
+    const aliveNeighbours = this.countAliveNeighbours(row, column);
+    const isAlive = this.currentGeneration[row][column] === 1;
+    const isDead = this.currentGeneration[row][column] === 0;
 
     if (isAlive) {
-      if (aliveNeihgbours < 2 || aliveNeihgbours > 3) {
-        this.nextGeneration[row][col] = 0;
-      } else if (aliveNeihgbours === 2 || aliveNeihgbours === 3) {
-        this.nextGeneration[row][col] = 1;
+      if (aliveNeighbours < 2 || aliveNeighbours > 3) {
+        this.nextGeneration[row][column] = 0;
+      } else if (aliveNeighbours === 2 || aliveNeighbours === 3) {
+        this.nextGeneration[row][column] = 1;
       }
     } else if (isDead) {
-      if (aliveNeihgbours === 3) {
-        this.nextGeneration[row][col] = 1;
+      if (aliveNeighbours === 3) {
+        this.nextGeneration[row][column] = 1;
       }
     }
     this.nextGenerationSubject.next(this.nextGeneration);
   }
 
-  private replaceGenerationAndResetGrid(): void {
-    for (let i = 0; i < GRID_DIMENSIONS.rows; i++) {
-      for (let j = 0; j < GRID_DIMENSIONS.cols; j++) {
+  private replaceGenerationAndResetGrid(columns = GRID_DIMENSIONS.cols, rows = GRID_DIMENSIONS.rows): void {
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < columns; j++) {
         this.currentGeneration[i][j] = this.nextGeneration[i][j];
         this.nextGeneration[i][j] = 0;
       }
